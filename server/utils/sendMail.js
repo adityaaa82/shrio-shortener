5f4111f7a1180654616142436cd6388c0bc61e89
Aditya Adavade <adityaa.sde@gmail.com>
2025-10-12 13:01:55 +0530

    fix mailsender

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -12,6 +12,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+   tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000, 
 });
 
 const template = `<!DOCTYPE html>
