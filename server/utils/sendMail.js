4f467d3bd1f47f60e0a93857e1dd8357fac4d7da
Aditya Adavade <adityaa.sde@gmail.com>
2025-10-12 13:06:38 +0530

    fix mailsender with port

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -6,8 +6,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: true,
+  port: Number(process.env.EMAIL_PORT) || 465,
+  secure: process.env.EMAIL_PORT == 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
